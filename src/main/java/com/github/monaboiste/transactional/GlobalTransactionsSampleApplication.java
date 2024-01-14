cb100ece5454e6e590ba2559308778410f9e27cd
Private <private@MAC-LQ7GMG97RG.local>
2024-01-14 12:56:13 +0100

    Init empty spring boot project

---
+++ src/main/java/com/github/monaboiste/transactional/GlobalTransactionsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.monaboiste.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalTransactionsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalTransactionsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/monaboiste/transactional/GlobalTransactionsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.monaboiste.transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalTransactionsSampleApplicationTests {
+
+	@Test
+	@SuppressWarnings("squid:S2699")
+	void context_loads() {
+	}
+}
