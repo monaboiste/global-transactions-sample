d0aeb6ac27e040cb4f1d3182b4b06083f77d2563
monaboiste <monikaszumna4@gmail.com>
2024-11-08 17:26:50 +0100

    Reorganize shared module

--- employee/infrastructure/src/main/java/com/github/monaboiste/transactional/domain/ValidatedEmail.java
+++ employee/infrastructure/src/main/java/com/github/monaboiste/transactional/domain/ValidatedEmail.java
@@ -2,6 +2,7 @@ package com.github.monaboiste.transactional.domain;
 
 /**
  * Rehydrated {@link Email} object.
+ * Module internal usage only.
  */
 public final class ValidatedEmail extends Email {
 
